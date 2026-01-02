1d1360a3a9211a088d6f963f45fab4545b036e8f
Jonathan ZAÏA <jojopo33@gmail.com>
2026-01-03 00:38:10 +0100

    Add variables for my color and text, add button to navbar to switch to darkmode

--- src/app/nav-bar/nav-bar.ts
+++ src/app/nav-bar/nav-bar.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -20,6 +20,27 @@ import { MatListModule } from '@angular/material/list';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar {
+export class NavBar implements OnInit {
     menuOpen = false;
+    isDarkMode = false;
+
+    ngOnInit() {
+      const savedMode = localStorage.getItem('darkMode');
+      this.isDarkMode = savedMode === 'true';
+      this.applyTheme();
+    }
+
+    toggleDarkMode() {
+      this.isDarkMode = !this.isDarkMode;
+      localStorage.setItem('darkMode', this.isDarkMode.toString());
+      this.applyTheme();
+    }
+
+    applyTheme() {
+      if (this.isDarkMode) {
+        document.body.classList.add('dark');
+      } else {
+        document.body.classList.remove('dark');
+      }
+    }
 }
